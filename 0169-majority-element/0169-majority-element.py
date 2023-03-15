1dd1d60d9f11cd1bb20f985c47c3d9f58c153f42
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-15 12:57:08 -0400

    Time: 132 ms (55.53%), Space: 15.1 MB (63.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,29 @@
 class Solution(object):
+#     def majorityElement(self, nums):
+#         num_dict = {}
+        
+#         for n in nums:
+#             if n not in num_dict:
+#                 num_dict[n] = 0
+            
+#             num_dict[n] += 1
+            
+#         return max(num_dict, key=num_dict.get)
+
     def majorityElement(self, nums):
         num_dict = {}
         
         for n in nums:
             if n not in num_dict:
                 num_dict[n] = 0
-            
+                
             num_dict[n] += 1
-            
-        return max(num_dict, key=num_dict.get)
+        
+        return max(num_dict, key = lambda k: num_dict[k])
+
         
         """
         :type nums: List[int]
         :rtype: int
         """
-        
+         
