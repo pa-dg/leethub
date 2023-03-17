dd9f7fa75333dc62ec10c48c6d5d00cf4cbca0b0
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-16 21:03:37 -0400

    Time: 88 ms (45.94%), Space: 21.2 MB (7.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -14,7 +14,7 @@ class Solution(object):
             freq[c].append(n)
             
         result = []
-        for i in range(len(nums), 0, -1):
+        for i in range(len(freq)-1, 0, -1):
             for n in freq[i]:
                 result.append(n)
                 if len(result) == k:
