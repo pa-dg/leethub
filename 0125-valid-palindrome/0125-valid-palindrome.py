8e88a00ef970635b75130726c6fb7363b49a6856
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-19 00:25:03 -0400

    Time: 60 ms (27.03%), Space: 14.6 MB (95.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,34 @@
 class Solution:
-    import re
     
+#Solution 1:
+#     import re
+
+#     def isPalindrome(self, s: str) -> bool:
+#         alphanum = re.sub(r'[^a-zA-Z0-9]',"", s.lower())
+
+#         return alphanum == alphanum[::-1]
+
+
+# #Solution 2:
     def isPalindrome(self, s: str) -> bool:
-        alphanum = re.sub(r'[^a-zA-Z0-9]',"", s.lower())
+        alpha = string.ascii_lowercase
+        num = set(str(i) for i in range(10))
+        left, right = 0, len(s) - 1
         
-        return alphanum == alphanum[::-1]
+        while left < right:
+            if s[left].lower() not in alpha and s[left] not in num:
+                left += 1
+                continue
+            if s[right].lower() not in alpha and s[right] not in num:
+                right -= 1
+                continue
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+                                            
+        
+            
+            
+    
