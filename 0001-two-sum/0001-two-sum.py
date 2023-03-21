1dfefcb9fc4c5c2ecde60eef20c989bdf68c3c0e
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-21 16:39:13 -0400

    Time: 65 ms (66.04%), Space: 15.2 MB (16.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        num_dict = {}
-
-        for index, n in enumerate(nums):
-            complement = target - n
-
-            if complement in num_dict:
-                return (index, num_dict[complement])
-
-            num_dict[n] = index
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
         
+        for i, n in enumerate(nums):
+            complement = target - n
+            
+            if complement in nums_dict:
+                return [i, nums_dict[complement]]
+            
+            nums_dict[n] = nums_dict.get(n, 0) + i
