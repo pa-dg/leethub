90fd5eeb464eb295962928b4ef7ffffbf01f9839
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-23 16:57:07 -0400

    Time: 149 ms (30.93%), Space: 69.5 MB (5.08%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,7 +3,16 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const previous = new Set(nums);
+    const previous = new Set();
     
-    return previous.size !== nums.length;
+    for (let i=0; i<nums.length; i++) {
+        if (previous.has(nums[i])){
+            return true;
+        }
+        previous.add(nums[i])
+    }
+    
+    console.log(previous)
+    
+    return false;
 };
