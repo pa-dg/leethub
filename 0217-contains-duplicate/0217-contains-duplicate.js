b37d1327977b0329b81ff0c79f111782dca9294d
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-23 16:44:37 -0400

    Time: 88 ms (86.37%), Space: 55.3 MB (30.76%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,11 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    // if (new Set(nums).size == nums.length){
-    //     return false;
-    // } else {
-    //     return true;
-    // }
     const previous = new Set(nums);
     
     return previous.size !== nums.length;
