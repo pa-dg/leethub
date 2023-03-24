062bae203d3658a8b60fcfba2defe57075cc1084
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-24 00:04:26 -0400

    Time: 76 ms (76.68%), Space: 44.9 MB (68.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,29 +4,48 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    const count = {}
+    let count = {}
     
     for (let n of nums) {
         count[n] = count[n] + 1 || 1;
     }
     
-    let sortable = [];
+//     let sortable = [];
     
-    for (var key in count) {
-        sortable.push([key, count[key]])
+//     for (var key in count) {
+//         sortable.push([key, count[key]])
+//     }
+    
+//     sortable.sort((a, b) => b[1] - a[1])
+//     console.log(sortable)
+    
+//     let result = [];
+//     for (let n of sortable) {
+//         if (result.length !== k) {
+//             result.push(n[0])
+//         }
+//     }
+
+//     return result
+    
+    let intByFreq = {};
+    
+    for (let num in count) {
+        if (!intByFreq[count[num]]) {
+            intByFreq[count[num]] = []
+        }
+        intByFreq[count[num]].push(num);
     }
     
-    sortable.sort((a, b) => b[1] - a[1])
-    console.log(sortable)
+    // console.log(intByFreq)
     
     let result = [];
-    for (let n of sortable) {
-        if (result.length !== k) {
-            result.push(n[0])
+    let i = nums.length;
+    while (result.length < k) {
+        if (intByFreq[i]) {
+            result = result.concat(intByFreq[i]);
         }
+        i--;
     }
-
     return result
-    
-    
 };
