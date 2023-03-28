c4b28ed75c50f266cb86a951e1a8a06c19503d66
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-27 22:44:05 -0400

    Time: 126 ms (15.73%), Space: 50.3 MB (34.66%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -12,20 +12,25 @@
 var reorderList = function(head) {
     let stack = [], node = head;
     
-    if (!node) return
+    if (!node) { return }
+    
     while (node) {
-    stack.push(node)
+        stack.push(node)
+        node = node.next
+    }
+
+    let len = stack.length;
+    node = head;
+    
+    for (let i = 0; i < len; i++) {
+    if (i % 2 === 0) {
+        node.next = stack.shift()
+    } else {
+        node.next = stack.pop()
+    }
     node = node.next
     }
 
-    let len = stack.length
-    node = head
-    for (let i = 0; i < len; i++) {
-    if (i % 2 === 0)
-      node.next = stack.shift()
-    else
-      node.next = stack.pop()
-    node = node.next
-    }
     node.next = null
+    
 };
