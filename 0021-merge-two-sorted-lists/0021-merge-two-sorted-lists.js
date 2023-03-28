ee95197d04940bac1043b1a0cb322826452cbc09
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-27 20:19:04 -0400

    Time: 70 ms (61.04%), Space: 44.3 MB (34.60%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,26 +11,50 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    const dummy = new ListNode();
-    let tail = dummy;
+// Solution 1 - iterative
+//     const dummy = new ListNode();
+//     let tail = dummy;
     
-    while (list1 && list2) {
-        if (list1.val < list2.val) {
-            tail.next = new ListNode(list1.val);
-            list1 = list1.next
-        } else {
-            tail.next = new ListNode(list2.val);
-            list2 = list2.next
-        }
+//     while (list1 && list2) {
+//         if (list1.val < list2.val) {
+//             tail.next = new ListNode(list1.val);
+//             list1 = list1.next
+//         } else {
+//             tail.next = new ListNode(list2.val);
+//             list2 = list2.next
+//         }
         
-        tail = tail.next;
+//         tail = tail.next;
+//     }
+    
+//     if (list1) {
+//         tail.next = list1;
+//     } else {
+//         tail.next = list2;
+//     }
+    
+//     return dummy.next;
+    
+// Solution 2 - recursion
+    if (!list1 && !list2) {
+        return null;
+    }
+    if (!list1) {
+        return list2;
+    }
+    if (!list2) {
+        return list1;
     }
     
-    if (list1) {
-        tail.next = list1;
+    if (list1.val < list2.val) {
+        let next = list1.next 
+        list1.next = mergeTwoLists(next, list2)
+        return list1
     } else {
-        tail.next = list2;
+        let next = list2.next
+        list2.next = mergeTwoLists(list1, next)
+        return list2
     }
     
-    return dummy.next;
+    
 };
