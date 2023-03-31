836a7604f75abb457cf0b1c19f4613d7a31b5cae
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-30 21:57:34 -0400

    Time: 66 ms (50.81%), Space: 43.1 MB (41.02%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  let dummy = new ListNode(0, head)
+  let left = dummy, right = head;
+  
+  while (n > 0) {
+    right = right.next;
+    n--;
+  }
+  
+  while (right) {
+    left = left.next;
+    right = right.next;
+  }
+  left.next = left.next.next;
+  return dummy.next;
+};
