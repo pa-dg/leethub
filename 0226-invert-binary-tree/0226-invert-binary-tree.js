687534d0bf4fa1a7c025e77ea61744c8f15d8596
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-31 11:07:07 -0400

    Time: 68 ms (33.26%), Space: 42.5 MB (23.00%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -10,18 +10,30 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
-    if (!root) return root;
+
+// Solution 1 - iterative
+// var invertTree = function(root) {
+//     if (!root) return root;
     
-    let stack = [ root ];
+//     let stack = [ root ];
     
-    while (stack.length > 0) {
-        const current = stack.pop();
+//     while (stack.length > 0) {
+//         const current = stack.pop();
         
-        if (current != null) {
-            [current.left, current.right] = [current.right, current.left]
-            stack.push(current.left, current.right)
-        }
-    }
-    return root;
+//         if (current != null) {
+//             [current.left, current.right] = [current.right, current.left]
+//             stack.push(current.left, current.right)
+//         }
+//     }
+//     return root;
+// };
+
+
+// Solution 2 - recursive
+var invertTree = function(root) {
+    if (!root) return null;
+    
+    const node = new TreeNode(root.val, invertTree(root.right), invertTree(root.left))
+    
+    return node;
 };
