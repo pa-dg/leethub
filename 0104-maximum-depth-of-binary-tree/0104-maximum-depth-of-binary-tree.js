7d40a669b59d73413e076ef7bfd8758b1d737672
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-31 14:24:43 -0400

    Time: 74 ms (41.03%), Space: 46.1 MB (8.37%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -10,8 +10,37 @@
  * @param {TreeNode} root
  * @return {number}
  */
+
+// Solution 1 - recursive DFS
+// var maxDepth = function(root) {
+//     if (!root) return 0;
+    
+//     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+// };
+
+
+// Solution 2 - iterative BFS
 var maxDepth = function(root) {
     if (!root) return 0;
     
-    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+    let queue = [ root ];
+    let depth = 0;
+    
+    while (queue.length) {
+        let len = queue.length; 
+        
+        for (let i=0; i<len; i++) {
+           let node = queue.shift();
+
+           if (node.left) {
+               queue.push(node.left)
+           }
+           if (node.right) {
+               queue.push(node.right)
+           }
+        }
+        depth++;
+    };
+    
+    return depth;
 };
