0cffe7839fb158284c2a06e4be1ab24315f8591d
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-03-30 22:04:18 -0400

    Time: 61 ms (74.64%), Space: 43.3 MB (34.26%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,18 +11,17 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-  let dummy = new ListNode(0, head)
-  let left = dummy, right = head;
+    let dummy = new ListNode(0, head);
+    let left = dummy, right = head;
   
-  while (n > 0) {
-    right = right.next;
-    n--;
-  }
-  
-  while (right) {
-    left = left.next;
-    right = right.next;
-  }
-  left.next = left.next.next;
-  return dummy.next;
+    while (n > 0) {
+        right = right.next;
+        n--;
+    }
+    while (right) {
+        right = right.next;
+        left = left.next;
+    }
+    left.next = left.next.next;
+    return dummy.next;
 };
