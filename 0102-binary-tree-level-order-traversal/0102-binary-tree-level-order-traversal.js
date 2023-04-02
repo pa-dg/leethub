c6df3a6131182558e6727354ee9ab442473b4843
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-04-02 00:35:45 -0400

    Time: 1218 ms (5.07%), Space: 52.7 MB (5.30%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.js
@@ -14,18 +14,29 @@ var levelOrder = function(root) {
     if (!root) return [];
     
     let queue = [ root ], res = [];
+    console.log('q', queue)
     
     while (queue.length) {
         let len = queue.length, row = [];
+        console.log('len', len)
         
         for (let i=0; i<len; i++) {
+            console.log('i', i)
             let current = queue.shift();
+            console.log('curr shift', current)
             row.push(current.val);
+            console.log('row', row)
             
             if (current.left) queue.push(current.left);
+            console.log('curr left', current.left)
             if (current.right) queue.push(current.right);
+            console.log('curr right', current.right)
+            console.log('current queue after children', queue)
+            console.log('qlen after children', len)
         }
         res.push(row)
+        console.log('res', res)
+        console.log('current queue', queue)
     }
     
     return res;
