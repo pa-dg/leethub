f05ade4378fd94f5387f17fd12d99cc72fe9373b
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-04-18 21:03:13 -0400

    Time: 60 ms (25.14%), Space: 13.9 MB (84.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -28,7 +28,6 @@ class Solution:
         while rotten and fresh > 0:
             for single in range(len(rotten)):
                 r, c = rotten.popleft()
-                print('pos', r, c)
 
                 for dir in directions:
                     neighborRow = r + dir[0]
@@ -39,12 +38,9 @@ class Solution:
                     if _isInbounds(neighborRow, neighborCol) and grid[neighborRow][neighborCol] == 1:
                         grid[neighborRow][neighborCol] = 2
                         rotten.append([neighborRow, neighborCol])
-                        print('rotten', rotten)
                         fresh -= 1
-                        print('fresh', fresh)
 
             minutes += 1
-            print('mins', minutes)  
                     
         return minutes if fresh == 0 else -1
                 
