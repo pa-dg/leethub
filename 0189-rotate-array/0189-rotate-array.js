68c9f55a5d150e5cbd1c5254e58e3fb5e3054205
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-05-02 17:55:45 -0400

    Time: 93 ms (67.75%), Space: 51 MB (91.59%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,10 +4,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    let result = [...nums]                                         
+//     let result = [...nums]                                         
     
-    result.map((n, idx) => {                                          
-        nums[(idx + k) % nums.length] = n         
-    })
+//     result.map((n, idx) => {                                          
+//         nums[(idx + k) % nums.length] = n         
+//     })
     
+    k %= nums.length;
+    
+    let reverse = function(i, j) {
+        while (i < j) {
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            i++;
+            j--;
+        }
+    }
+    
+    reverse(0, nums.length - 1);
+    reverse(0, k-1);
+    reverse(k, nums.length - 1)
 };
