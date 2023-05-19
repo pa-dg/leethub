f334ef764114be9af55180d315f94e48e37e113d
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-05-19 16:10:57 -0400

    Time: 62 ms (67.53%), Space: 45.4 MB (19.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0, r = nums.length - 1
+    let left = 0, right = nums.length - 1;
     
-    while (l <= r) {    
-        const midIdx = Math.floor(l + (r - l) / 2);
-        const mid = nums[midIdx]
+    while (left <= right) {
+        const midIdx = Math.floor(left + (right - left) / 2);
+        const midVal = nums[midIdx];
         
-        if (mid > target) {
-            r = midIdx - 1;
-        } else if (mid < target) {
-            l = midIdx + 1;
-        } else {
+        if (midVal === target) {
             return midIdx;
+        } else if (midVal > target) {
+            right = midIdx - 1;
+        } else {
+            left = midIdx + 1;
         }
     };
     
-    return -1
+    return -1;
 };
