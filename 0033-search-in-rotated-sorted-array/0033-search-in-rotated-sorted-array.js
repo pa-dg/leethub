644d8541d017b35369d6a503b87092c8af2a1d2f
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-05-23 12:01:16 -0400

    Time: 94 ms (5.08%), Space: 44.7 MB (5.54%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,29 +4,31 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0;
-    let r = nums.length - 1;
-    
-    while (l <= r) {
-        const m = Math.floor((l + r) / 2);
-        if (nums[m] === target) {
-            return m;
-        }
-        
-        if (nums[m] >= nums[l]) {
-            if (nums[l] <= target && nums[m] > target) {
-                r = m - 1;
-            } else {
-                l = m + 1;
-            }
-        } else {
-            if (nums[r] >= target && nums[m] < target) {
-                l = m + 1;
-            } else {
-                r = m - 1;
-            }
-        }
-    }
-    
-    return -1;
+  let [left, right] = [0, nums.length - 1];
+
+  while (left <= right) {
+    console.log('left', left)
+    console.log('right', right)
+    let midIdx = Math.floor(left + (right - left) / 2);
+    console.log('mid', midIdx)
+    let midVal = nums[midIdx];
+
+    if (midVal === target) {
+      return midIdx;
+    } else if (nums[left] <= midVal) { // left to mid is non-decreasing
+      if (target < midVal && nums[left] <= target) { // target is on left side of mid
+        right = midIdx - 1;
+      } else { // target is on right side of mid (whether sorted or not!)
+        left = midIdx + 1;
+      }
+    } else { // mid to right is sorted
+      if (target > midVal && nums[right] >= target) { // target is on right side of mid
+        left = midIdx + 1; 
+      } else { // target is on left side of mid
+        right = midIdx - 1;
+      }
+    }         
+  }
+
+  return -1;
 };
