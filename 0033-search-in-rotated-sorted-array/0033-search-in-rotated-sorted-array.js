984064699fa885cf65323e62e2b0799db5746c8a
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-05-23 22:27:37 -0400

    Time: 64 ms (34.56%), Space: 41.8 MB (77.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,34 +4,6 @@
  * @return {number}
  */
 var search = function(nums, target) {
-//   let [left, right] = [0, nums.length - 1];
-
-//   while (left <= right) {
-//     // console.log('left', left)
-//     // console.log('right', right)
-//     let midIdx = Math.floor(left + (right - left) / 2);
-//     // console.log('mid', midIdx)
-//     let midVal = nums[midIdx];
-
-//     if (midVal === target) {
-//       return midIdx;
-//     } else if (nums[left] <= midVal) { // left to mid is non-decreasing
-//       if (target < midVal && nums[left] <= target) { // target is on left side of mid
-//         right = midIdx - 1;
-//       } else { // target is on right side of mid (whether sorted or not!)
-//         left = midIdx + 1;
-//       }
-//     } else { // mid to right is sorted
-//       if (target > midVal && nums[right] >= target) { // target is on right side of mid
-//         left = midIdx + 1; 
-//       } else { // target is on left side of mid
-//         right = midIdx - 1;
-//       }
-//     }         
-//   }
-
-//   return -1;
-    
     let [l, r] = [0, nums.length - 1];
     
     while (l <= r) {
