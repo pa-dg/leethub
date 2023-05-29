47bb0db80452ccafb37b5a24a2b488e5d63039d9
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-05-28 23:38:12 -0400

    Time: 127 ms (88.85%), Space: 69.6 MB (70.18%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let [slow, fast] = [head, head];
+    
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    };
+    
+    let prev = null;
+    while (slow) {
+        let next = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = next;
+    };
+    
+    let [left, right] = [head, prev]
+    while (right) {
+        if (left.val !== right.val) {
+          return false;
+        }
+        
+        left = left.next;
+        right = right.next;
+    };
+    
+    return true;
+};
