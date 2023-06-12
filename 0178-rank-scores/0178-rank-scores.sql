810e1a73dcc084dea31c773f264965952a2cbb9c
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-06-11 23:50:10 -0400

    Time: 879 ms (12.97%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-SELECT S1.Score, (
-    SELECT COUNT(DISTINCT Score) FROM Scores WHERE Score >= S1.Score) AS `Rank`
-FROM Scores S1
-ORDER BY S1.Score DESC
+SELECT S.Score, COUNT(S2.Score) as `Rank`
+FROM Scores S,
+(SELECT DISTINCT Score FROM Scores) S2
+WHERE S.Score<=S2.Score
+GROUP BY S.Id
+ORDER BY S.Score DESC;
+
+# Other solution:
+# SELECT S1.Score, (
+#     SELECT COUNT(DISTINCT Score) FROM Scores WHERE Score >= S1.Score) AS Rank
+# FROM Scores S1
+# ORDER BY S1.Score DESC
