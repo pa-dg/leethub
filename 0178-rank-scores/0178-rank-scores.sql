aa18710f7303c9998b114b06aed1a7830c7f238d
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-06-13 00:27:50 -0400

    Time: 563 ms (56.22%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -6,8 +6,3 @@ WHERE S1.Score <= S2.Score
 GROUP BY S1.Id
 ORDER BY S1.Score DESC;
 
-# Other solution:
-# SELECT S1.Score, (
-#     SELECT COUNT(DISTINCT Score) FROM Scores WHERE Score >= S1.Score) AS Rank
-# FROM Scores S1
-# ORDER BY S1.Score DESC
