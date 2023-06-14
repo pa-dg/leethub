fea9955dbd6bedee04be593af71de49e1b90012b
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2023-06-13 23:36:41 -0400

    Time: 1411 ms (45.33%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,20 +1,13 @@
 # Write your MySQL query statement below
-# SELECT D.name AS Department, E.name AS Employee, E.salary AS Salary
-# FROM Employee E
-# JOIN Department D ON E.departmentId = D.id
-# GROUP BY D.name
-# HAVING 
-#     MAX(Salary)
-
-
 SELECT D.name AS Department, E.name AS Employee, E.salary AS Salary
-FROM 
-    Employee E,
-    (SELECT departmentId, MAX(salary) AS maxSal FROM Employee GROUP BY departmentId) T,
-    Department D
-WHERE E.departmentId = T.departmentId
-    AND E.salary = T.maxSal
-    AND E.departmentId = D.id
+FROM Employee E
+JOIN Department D ON E.departmentId = D.id
+WHERE (E.departmentId, E.salary) IN
+        (SELECT departmentId, MAX(Salary)
+         FROM Employee
+         GROUP BY departmentId)
+;
+
 
 # departmentID is the foreign key of the ID from the Department table
 # guaranteed department name is NOT NULL
