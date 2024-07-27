1b2d533d43e07af2168a321b6f39d33bf6f252dc
Patricia Andrea de Guzman <103235494+pa-dg@users.noreply.github.com>
2024-07-27 00:18:42 -0400

    Time: 66 ms (82.49%), Space: 53.1 MB (74.95%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.ts
@@ -0,0 +1,30 @@
+// use two pointers to set range of search from 1 to maximum bananas in piles
+// using while..loop to determine k, by finding midIdx, midVal of the range to start with
+// determine how many bananas given k from previous step
+// 
+
+function minEatingSpeed(piles: number[], h: number): number {
+    function canFinish(speed: number): boolean {
+        let hours = 0;
+        for (let pile of piles) {
+            hours += Math.ceil(pile/speed)
+        }
+        return hours <= h
+        
+    }
+    
+    let low = 1, high = Math.max(...piles)
+    
+    while (low <= high) {
+        const speed = Math.floor(low + (high - low)/2)
+        
+        if (canFinish(speed)) {
+            high = speed - 1
+        } else {
+            low = speed + 1
+        }
+    }
+    
+    return low;
+    
+};
